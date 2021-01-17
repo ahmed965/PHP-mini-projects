74c32b404ff0556795b337caed414032c5aa9a9d
ahmed <56020324+ahmed965@users.noreply.github.com>
2021-01-17 16:54:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,22 @@ include'classes/calcul.class.php';
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-<form action="calcul.php" method="post">
-	<h2>Claculator</h2>
-	<input type="number" name="num1" placeholder="first number">
-	<select name="operator">
-		<option value="add">Addition</option>
-		<option value="sub">Substraction</option>
-		<option value="div">Division</option>
-		<option value="mul">Multiplication</option>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<form action="calcul.php" method="post">
+			<h2>Claculator</h2>
+			<input type="number" name="num1" placeholder="first number">
+			<select name="operator">
+				<option value="add">Addition</option>
+				<option value="sub">Substraction</option>
+				<option value="div">Division</option>
+				<option value="mul">Multiplication</option>
 
-	</select>
-	<input type="number" name="num2" placeholder="Second number">
-	<input type="submit" name="submit" value="Calculate">
-</form>
-</body>
+			</select>
+			<input type="number" name="num2" placeholder="Second number">
+			<input type="submit" name="submit" value="Calculate">
+		</form>
+	</body>
 </html>
